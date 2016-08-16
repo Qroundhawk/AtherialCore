7e60aa5c4f7a30586b4a8878df3518f5cc4d6f97
Qroundhawk <t0mmy4@hotmail.com>
2016-08-16 21:16:49 +0200

    Spigot added

--- AtherialCore/src/me/TomAlex/AtherialRunes/Main.java
+++ AtherialCore/src/me/TomAlex/AtherialRunes/Main.java
@@ -1,7 +1,25 @@
 package me.TomAlex.AtherialRunes;
 
-public class Main 
-{
-	String test = "Test";
+import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener 
+{
+	public static Logger log = Bukkit.getLogger();
+
+	public static void out(String s) 
+	{
+		log.info("Test" + s);
+	}
+
+	public void onEnable() {
+		out("Plugin enabled");
+		saveDefaultConfig();
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
+	}
 }
