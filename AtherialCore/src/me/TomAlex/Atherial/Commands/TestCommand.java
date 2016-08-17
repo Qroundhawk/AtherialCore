8573ab4f044ac808e567e8afa43cbba9d9fa470d
ALEKOS <ALEKOS@192.168.2.2>
2016-08-17 18:00:04 +0200

    1

---
+++ AtherialCore/src/me/TomAlex/Atherial/Commands/TestCommand.java
@@ -0,0 +1,29 @@
+package me.TomAlex.Atherial.Commands;
+
+import me.TomAlex.Atherial.SettingsManager;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class TestCommand implements CommandExecutor {
+	
+	SettingsManager settings = SettingsManager.getInstance();
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		
+		Player p = (Player) sender;
+		
+		if (!p.hasPermission("test.command")) {
+			p.sendMessage(ChatColor.RED + "You do not have permissions to do that.");
+			return true;
+		}
+		
+		
+		
+		return true;
+	}
+
+}
--- AtherialCore/src/me/TomAlex/Atherial/Main.java
+++ AtherialCore/src/me/TomAlex/Atherial/Main.java
@@ -2,10 +2,12 @@ package me.TomAlex.Atherial;
 
 import me.TomAlex.Atherial.SettingsManager;
 import me.TomAlex.Atherial.Combat.PlayerJoinSet;
+import me.TomAlex.Atherial.Commands.TestCommand;
 
 import java.util.HashMap;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,6 +23,10 @@ public class Main extends JavaPlugin implements Listener {
 		pm.registerEvents(this, this);
 		pm.registerEvents(new PlayerJoinSet(), this);
 		
+		Bukkit.getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+		
+		
+		getCommand("test").setExecutor(new TestCommand());
 	}
 	
 	public void onDisable() {
