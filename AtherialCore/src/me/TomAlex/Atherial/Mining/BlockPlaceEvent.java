dfb2e49e515fa72e4534e1083279ba4f86de9356
ALEKOS <ALEKOS@192.168.2.2>
2016-08-17 22:11:14 +0200

    1

--- AtherialCore/src/me/TomAlex/Atherial/Mining/BlockPlaceEvent.java
+++ AtherialCore/src/me/TomAlex/Atherial/Mining/BlockPlaceEvent.java
@@ -3,14 +3,10 @@ package me.TomAlex.Atherial.Mining;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import me.TomAlex.Atherial.ArmorEquipEvent.ArmorEquipEvent;
-
 public class BlockPlaceEvent implements Listener 
 {
 	@EventHandler
-	public void Place(BlockPlaceEvent e) 
-	{
-		
+	public void Place(BlockPlaceEvent e) {
 	}
 
 }
