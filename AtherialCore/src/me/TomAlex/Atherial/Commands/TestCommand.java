ddf4788a7bc447caf194d9aea52eaa064fdf6124
Qroundhawk <t0mmy4@hotmail.com>
2016-08-18 22:53:11 +0200

    wew

--- AtherialCore/src/me/TomAlex/Atherial/Commands/TestCommand.java
+++ AtherialCore/src/me/TomAlex/Atherial/Commands/TestCommand.java
@@ -2,6 +2,8 @@ package me.TomAlex.Atherial.Commands;
 
 import me.TomAlex.Atherial.SettingsManager;
 
+import java.util.Scanner;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -20,12 +22,20 @@ public class TestCommand implements CommandExecutor {
 			p.sendMessage(ChatColor.RED + "You do not have permissions to do that.");
 			return true;
 		}
+		
+		String loreArmor = p.getItemInHand().getItemMeta().getLore().get(1);
+		Scanner in1 = new Scanner(loreArmor).useDelimiter("[^0-9]+");
+		int a = in1.nextInt();
+		int a1 = in1.nextInt();
+		p.sendMessage("a"  + a + " " + a1 + " "  + " " +  " ");
+		p.sendMessage(""+ p.getItemInHand().getItemMeta().getLore().size());
+		
 			//String chatcolor = ""+ ChatColor.RED;
 			//p.sendMessage(chatcolor+ "Loresize: "  + p.getItemInHand().getItemMeta().getLore().size());
 			//String loreThorns = p.getItemInHand().getItemMeta().getLore().get(7);
 			//p.sendMessage("" + loreThorns.indexOf(1));
 			
-			p.sendMessage("" + settings.getOreData().getDouble("coalore.coal1.loc"));
+
 		
 		
 		
