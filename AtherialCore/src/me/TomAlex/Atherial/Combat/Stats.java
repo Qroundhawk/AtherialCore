79c5c42a3c69853c30e97dcaf4f3255d9dff6c76
Qroundhawk <t0mmy4@hotmail.com>
2016-08-19 15:04:18 +0200

    ew

--- AtherialCore/src/me/TomAlex/Atherial/Combat/Stats.java
+++ AtherialCore/src/me/TomAlex/Atherial/Combat/Stats.java
@@ -1,5 +1,8 @@
 package me.TomAlex.Atherial.Combat;
 
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -7,13 +10,23 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Stats implements Listener
 {
 	@EventHandler
 	public void statsMenu(InventoryClickEvent e)
 	{
-		Player p = (Player) e.getWhoClicked();		
+		Player p = (Player) e.getWhoClicked();	
+		
+		if (e.getInventory().getName().equals(ChatColor.DARK_GREEN + ChatColor.BOLD.toString() + "Player Menu")) 
+		{	
+			e.setCancelled(true);
+		}
+		
 		if(!(e.getSlotType().toString() == "QUICKBAR"))
 		{
 			return;
@@ -32,7 +45,29 @@ public class Stats implements Listener
 		{
 			e.getPlayer().sendMessage(ChatColor.RED + "You can't drop this item");
 			e.setCancelled(true);
-		}
+		}		
+	}
+	@EventHandler
+	public void Compassdrop(PlayerInteractEvent e)
+	{
+		Player p = e.getPlayer();
 		
+		if(e.getItem().getType() == Material.COMPASS)
+		{
+			Inventory menu = Bukkit.createInventory(null, 18,
+					ChatColor.DARK_GREEN+ ChatColor.BOLD.toString() + "Player Menu");
+			
+
+			ItemStack slotA = new ItemStack(Material.BOOK);
+			ItemMeta slotAMeta = slotA.getItemMeta();
+			slotAMeta.setDisplayName(ChatColor.RED + ChatColor.BOLD.toString()+ "STATS");
+			slotAMeta.setLore(Arrays.asList(ChatColor.DARK_GREEN + "", ChatColor.DARK_GREEN + ""));
+			slotA.setItemMeta(slotAMeta);
+	
+			menu.setItem(0, slotA);
+
+			p.openInventory(menu);
+			
+		}else return;
 	}
 }
