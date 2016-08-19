871042dacf7236ae274f753144cdae799cd349d7
ALEKOS <ALEKOS@192.168.2.2>
2016-08-19 22:46:05 +0200

    2

--- AtherialCore/src/me/TomAlex/Atherial/Mining/OreRespawn.java
+++ AtherialCore/src/me/TomAlex/Atherial/Mining/OreRespawn.java
@@ -30,64 +30,6 @@ public class OreRespawn {
 			public void run() {
 				
 				//@Coal respawn
-				/*if (settings.Coal.isEmpty() == false) {
-					for (Entry<String, Integer> i : settings.Coal.entrySet()) {
-						
-						if (!(settings.getOreData().get("coalore." + i.getKey()) == null)) {
-							
-							Location loc = (Location) settings.getOreData().get("coalore." + i.getKey() + ".loc");
-							
-							
-							settings.Coal.put(i.getKey(), i.getValue() - 5);
-							
-							
-							if 	(i.getValue() <= 0) {
-								if (!(loc.getWorld().getBlockAt(loc).getType() == Material.COAL_ORE)) {
-									loc.getWorld().getBlockAt(loc).setType(Material.COAL_ORE);
-								}
-								settings.Coal.remove(i.getKey());
-								
-								
-							}
-						}else{
-							settings.Coal.remove(i.getKey());
-						}
-					}
-				}*/
-				
-				
-				//@Coal respawn
-				/*if (settings.Coal.isEmpty() == false) {
-					for (Entry<String, Integer> i : settings.Coal.entrySet()) {
-						i.setValue(i.getValue() - 5);
-						
-						
-						if (settings.getOreData().get("coalore." + i.getKey()) == null) {
-							settings.Coal.remove(i.getKey());
-						}else{
-							Location loc = (Location) settings.getOreData().get("coalore." + i.getKey() + ".loc");
-							
-							if (i.getValue() == 0) {
-								loc.getWorld().getBlockAt(loc).setType(Material.COAL_ORE);
-							}
-						}
-					}
-					
-					List<String> toRemove = new ArrayList<String>();
-					for (Entry<String, Integer> i : settings.Coal.entrySet()) {
-						if (i.getValue() <= 0) {
-							if (!toRemove.contains(i.getKey())) {
-								toRemove.add(i.getKey());
-							}
-						}
-					}
-					for (String i : toRemove) {
-						settings.Coal.remove(i);
-					}
-				}*/
-				
-				
-				
 				if (settings.Coal.isEmpty() == false) {
 					List<String> toRemove = new ArrayList<String>();
 					for (Entry<String, Integer> i : settings.Coal.entrySet()) {
